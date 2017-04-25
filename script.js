8108dcfd3b5e3fd018a3fe65a1f67eb455806162
Mark Danovich <mark.danovich@gmail.com>
2017-04-25 22:50:04 +0100

    script

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ function redraw(){
   
   context.strokeStyle = "#000000";
   context.lineJoin = "round";
-  context.lineWidth = 1;
+  context.lineWidth = 2;
 			
   for(var i=0; i < clickX.length; i++) {		
     context.beginPath();
