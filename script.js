b6fcd2461dad6d0837ad39696e3751624b111f25
Mark Danovich <mark.danovich@gmail.com>
2017-04-27 00:21:19 +0100

    list

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ function predict(input,wih,who){
 	//console.log(sf);
 	for (var i=0; i<10; i++){
 		var s=i.toString();
-		$('#v'+s).html(arrdigits[i].toString().match(/^-?\d+(?:\.\d{0,2})?/)[0]);
+		$('#v'+s).html(arrdigits[i].toString().match(/^-?\d+(?:\.\d{0,3})?/)[0]);
 	}
 
 
