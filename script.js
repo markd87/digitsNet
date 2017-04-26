7d960ec1f05b0539268d6afff95e2c8168c4d651
Mark Danovich <mark.danovich@gmail.com>
2017-04-26 20:54:03 +0100

    clear

--- script.js
+++ script.js
@@ -174,8 +174,8 @@ function redraw(){
   context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
   
   context.strokeStyle = "#000000";
-  context.lineJoin = "round";
-  context.lineWidth = 4;
+  //context.lineJoin = "round";
+  context.lineWidth = 5;
 			
   for(var i=0; i < clickX.length; i++) {		
     context.beginPath();
