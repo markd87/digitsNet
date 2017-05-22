02189cca4350fdcaf88c8b147ad1b9ab39609b07
Mark Danovich <mark.danovich@gmail.com>
2017-05-22 20:51:28 +0100

    loading gif

--- script.js
+++ script.js
@@ -183,7 +183,8 @@ $("#predict").attr('disabled','disabled');
 });
 
 $('#predict').mousedown(function(e){
-	ֱ$("span#predres").html("<img src='https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif'/>");
+
+	$("span#predres").html("<img src='https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif'/>");
 
 	destCtx.clearRect(0, 0, canvas.width, canvas.height);
 
